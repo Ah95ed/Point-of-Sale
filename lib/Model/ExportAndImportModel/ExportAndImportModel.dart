9bd3a7d9f2d397aac4107465367f83a10d0737ea
Ahmed Shaker <amhmeed31@gmail.com>
2025-01-05 20:59:58 +0300

    done export  data

--- lib/Model/ExportAndImportModel/ExportAndImportModel.dart
+++ lib/Model/ExportAndImportModel/ExportAndImportModel.dart
@@ -3,8 +3,6 @@ import 'dart:io';
 import 'package:csv/csv.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_file_dialog/flutter_file_dialog.dart';
-import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:point_of_sell/Helper/Log/LogApp.dart';
 import 'package:point_of_sell/Model/Models/DataBaseApp/DataBaseSqflite.dart';
@@ -17,53 +15,74 @@ class ExportAndImportModel {
   }
 
   Future<void> exportData(BuildContext context) async {
-    List t = [];
-    final data = await _database.getAllData();
-    final ls = await data.map((row) {
-      t.add(row![DataBaseSqflite.name]);
-      t.add(row[DataBaseSqflite.codeItem]);
-      t.add(row[DataBaseSqflite.buy]);
-      t.add(row[DataBaseSqflite.sale]);
-      t.add(row[DataBaseSqflite.quantity]);
-      t.add(row[DataBaseSqflite.date]);
-      t.add(row[DataBaseSqflite.company]);
-      t.add(row[DataBaseSqflite.id]);
-      return t.toList();
-    }).toList();
-    final params = OpenFileDialogParams(
-      dialogType: OpenFileDialogType.document,
-    );
-    DirectoryLocation? result = await FlutterFileDialog.pickDirectory();
- 
-    String? csvString = const ListToCsvConverter().convert(ls);
-    File exportFile = File(result.toString());
-    await exportFile.writeAsString(csvString).whenComplete(() {
-      logSuccess('message export success');
-    });
-    //   logSuccess('message export success');
-    if (result != null) {
-      logSuccess('Selected folder path: $result');
-    } else {
-      logError('No folder selected');
-    }
-    // if (ls.isEmpty) {
-    //   logWarning('message export empty');
-    //   return;
-    // }
+    await Permission.accessMediaLocation.request();
+    // var status = await Permission.storage.request();
+    var status = await Permission.manageExternalStorage.request();
+     List t = [];
+      final data = await _database.getAllData();
+      final ls = await data.map((row) {
+        t.add(row![DataBaseSqflite.name]);
+        t.add(row[DataBaseSqflite.codeItem]);
+        t.add(row[DataBaseSqflite.buy]);
+        t.add(row[DataBaseSqflite.sale]);
+        t.add(row[DataBaseSqflite.quantity]);
+        t.add(row[DataBaseSqflite.date]);
+        t.add(row[DataBaseSqflite.company]);
+        t.add(row[DataBaseSqflite.id]);
+        return t.toList();
+      }).toList();
 
-    // String? csvString = const ListToCsvConverter().convert(ls);
-    // // السماح للمستخدم بتحديد المسار
+    if (status.isGranted) {
+      String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
+
+      if (selectedDirectory != null) {
+        String filePath = '$selectedDirectory/output.csv';
+
+        // Sample CSV data
+        
+
+        String csvString = const ListToCsvConverter().convert(ls);
+
+        // Save CSV to the selected path
+        File file = File(filePath);
+        await file.writeAsString(csvString).whenComplete(() {
+          logSuccess('CSV saved at: $filePath');
+        });
+        logSuccess('CSV saved at: $filePath');
+      } else {
+        print('Directory selection canceled.');
+      }
+    } else {
+      logError('Storage permission denied.');
+      return;
+    }
+
+    
+
+    //   if (ls.isEmpty) {
+    //     logWarning('message export empty');
+    //     return;
+    //   }
+
+    //   String? csvString = const ListToCsvConverter().convert(ls);
+    //   Directory directory = await getApplicationDocumentsDirectory();
+    // String filePath = '${directory.path}/output.csv';
+
+    // File file = File(filePath);
+    // await file.writeAsString(csvString);
+    // logSuccess(" Exported to $filePath \n ${file.path}");
+    // السماح للمستخدم بتحديد المسار
     // logWarning('message export empty 1');
-    // String savePath = await FilePicker.platform.getDirectoryPath(
-    //   dialogTitle: 'Hi '
-    // ) ?? '';
+    // String savePath =
+    //     await FilePicker.platform.getDirectoryPath(dialogTitle: 'Hi ') ?? '';
     // logWarning('message export empty 2');
     // // حفظ الملف في المسار الذي اختاره المستخدم
     // File exportFile = File(savePath);
-    // await exportFile.writeAsString(csvString).whenComplete(() async {
-    //   logSuccess('message export success');
+    // await exportFile.writeAsString(savePath).then((f) {
+    //   logWarning('message export empty 3 ${f.path}');
+    // }).catchError((error) {
+    //   logError('message export empty 4');
     // });
-    // logWarning('message export empty 3');
   }
 
   Future<void> importData() async {
